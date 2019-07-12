408d96508af7c62ea7721a515da6b03d4aed0b4e
eliorfrig <2493243+EliorFrig@users.noreply.github.com>
2019-07-12 14:53:11 +0300

    data_utils: Reorganized code. Use more of pandas features (e.g. query vs pure python filtering). Removed "dead" calls (e.g. sort_index without catching the result). Added a private PreProcessor class with static and private methods. Added more logging and run params.

--- data_utils.py
+++ data_utils.py
@@ -1,156 +1,181 @@
 import json
 from data.uri import *
 
+import sys
 import logging
 import pandas as pd
 import argparse
+import pathlib
 
 VERSION = '0.0.1'
 CONCEPTNET_VERSION = '5.7.0'
+NUMBERBATCH_VERSION = '17.06'
 DEFAULT_DATASET_PATH = 'data/conceptnet-assertions-{}.csv.gz'.format(CONCEPTNET_VERSION)
-DEFAULT_PROCESSED_DATASET_PATH = 'out/conceptnet-{}.h5'.format(CONCEPTNET_VERSION)
-
-FILEPATH = "data/conceptnet-assertions-5.6.0.csv.gz"
-NUMBERBATCH = 'weights/conceptnet-numberbatch.h5'
-
-def load_data_iterator(filepath=FILEPATH, chunksize=20000):
-    return pd.read_csv(filepath, delimiter='\t', chunksize=chunksize,
-                       names=['uri', 'relation', 'subject', 'object', 'data'])
+DEFAULT_NORMALIZED_DATASET_PATH = 'out/normalized_conceptnet-{}.h5'.format(CONCEPTNET_VERSION)
+DEFAULT_SEQ2SENT_DATASET_PATH = 'out/seq2sent_conceptnet-{}.h5'.format(CONCEPTNET_VERSION)
+DEFAULT_NUMBERBATCH_PATH = 'data/conceptnet-numberbatch-mini-{}.h5'.format(NUMBERBATCH_VERSION)
 
 
-def create_input_and_target_from_df(df):
-    df = filter_df(df)
-    x = df['subject'] + " " + df['relation'] + " " + df['object']
-    y = "<start> " + df['text'] + ' <end>'
-    return x, y
+class _PreProcessor:
+    def __init__(self, dataset, normalized_dataset_path, seq2sent_dataset_path):
+        self.dataset = pathlib.Path(dataset)
+        self.normalized_dataset_path = pathlib.Path(normalized_dataset_path)
+        self.seq2sent_dataset_path = pathlib.Path(seq2sent_dataset_path)
+        self.normalized_df = None
+
+    def make_normalized_dataset(self, override, filter_lines):
+        logging.info('Making normalized dataset {} into {}'.format(self.dataset, self.normalized_dataset_path))
+        if not self.dataset.exists():
+            self._fail('Input dataset {} does not exist'.format(self.dataset))
+        if self.normalized_dataset_path.exists():
+            if not override:
+                self._fail('Output normalized dataset {} already exist, use \'--override\' to override it'.format(
+                    self.normalized_dataset_path))
+            else:
+                logging.info('Overriding output normalized dataset {} since \'--override\' is given'.format(
+                    self.normalized_dataset_path))
+        self.normalized_dataset_path.parent.mkdir(parents=True, exist_ok=True)
+        self.normalized_df = pd.concat(
+            [self._filter_df(self._normalize_df(chunk)) for chunk in self._iterate_dataset(filter_lines)])
+        self.normalized_df.to_hdf(self.normalized_dataset_path, 'data', mode='w')
+        logging.info('Done making normalized dataset')
+
+    def make_seq2sent_dataset(self, override, filter_lines):
+        logging.info('Making Seq2Sent dataset {} into {}'.format(self.dataset, self.seq2sent_dataset_path))
+        if not self.dataset.exists():
+            self._fail('Input dataset {} does not exist'.format(self.dataset))
+        if self.seq2sent_dataset_path.exists():
+            if not override:
+                self._fail('Output Seq2Sent dataset {} already exist, use \'--override\' to override it'.format(
+                    self.seq2sent_dataset_path))
+            else:
+                logging.info('Overriding output Seq2Sent dataset {} since \'--override\' is given'.format(
+                    self.seq2sent_dataset_path))
+        self.seq2sent_dataset_path.parent.mkdir(parents=True, exist_ok=True)
+
+        if self.normalized_df is None:
+            if override or not self.normalized_dataset_path.exists():
+                self.make_normalized_dataset(override, filter_lines)
+            else:
+                self.normalized_df = load_normalized_dataset(self.normalized_dataset_path)
+
+        inputs, targets = self._create_input_and_target_from_df(self.normalized_df)
+        inputs.to_hdf(self.seq2sent_dataset_path, 'inputs', mode='w')
+        targets.to_hdf(self.seq2sent_dataset_path, 'targets', mode='a')
+        logging.info('Done making Seq2Sent dataset')
+
+    def _iterate_dataset(self, filter_lines):
+        return pd.read_csv(self.dataset, delimiter='\t', chunksize=50000,
+                           names=['uri', 'relation', 'subject', 'object', 'data'],
+                           skiprows=lambda x: x % filter_lines != 0)
+
+    @staticmethod
+    def _normalize_df(df):
+        new_df = pd.DataFrame()
+        new_df['relation'] = df['relation'].apply(uri_to_label)
+        new_df['language'] = df['uri'].apply(get_uri_language)
+        new_df['subject'] = df['subject'].apply(uri_to_label)
+        new_df['subject_lang'] = df['subject'].apply(get_uri_language)
+        new_df['object'] = df['object'].apply(uri_to_label)
+        new_df['object_lang'] = df['object'].apply(get_uri_language)
+        new_df['data'] = df['data'].apply(json.loads)
+        new_df['dataset'] = new_df['data'].apply(lambda x: x['dataset'])
+        new_df['weight'] = new_df['data'].apply(lambda x: x['weight'])
+        new_df['text'] = new_df['data'].apply(lambda x: x.get('surfaceText', '').replace('[', '').replace(']', ''))
+        return new_df
+
+    @staticmethod
+    def _filter_df(df, include_empty_text=False):
+        df.query('language=="en" and subject_lang=="en" and object_lang=="en"', inplace=True)
+        df.dropna(subset=['subject', 'object'], inplace=True)
+        if not include_empty_text:
+            df.query('text != ""', inplace=True)
+        return df
+
+    @staticmethod
+    def _create_input_and_target_from_df(df):
+        x = df['subject'] + ' ' + df['relation'] + ' ' + df['object']
+        y = '<start> ' + df['text'] + ' <end>'
+        return x, y
+
+    @staticmethod
+    def _fail(err_msg):
+        logging.error(err_msg)
+        sys.exit(1)
 
 
-def filter_df(df, include_empty_text=False):
-    df = df[(df['language'] == 'en') & (df['subject_lang'] == 'en') & (df['object_lang'] == 'en')]
-    df = df[(df['subject'].notna()) & (df['object'].notna())]
-    if not include_empty_text:
-        df = df[df['text'] != ""]
-    df.reset_index()
-    return df
+def load_normalized_dataset(h5_path=DEFAULT_NORMALIZED_DATASET_PATH):
+    return pd.read_hdf(h5_path, 'data')
 
 
-def normalize_df(df):
-    new_df = pd.DataFrame()
-    new_df['relation'] = df['relation'].apply(uri_to_label)
-    new_df['language'] = df['uri'].apply(get_uri_language)
-    new_df['subject'] = df['subject'].apply(uri_to_label)
-    new_df['subject_lang'] = df['subject'].apply(get_uri_language)
-    new_df['object'] = df['object'].apply(uri_to_label)
-    new_df['object_lang'] = df['object'].apply(get_uri_language)
-    new_df['data'] = df['data'].apply(json.loads)
-    new_df['dataset'] = new_df['data'].apply(lambda x: x['dataset'])
-    new_df['weight'] = new_df['data'].apply(lambda x: x['weight'])
-    new_df['text'] = new_df['data'].apply(lambda x: x.get('surfaceText', '').replace('[', '').replace(']', ''))
-
-    return new_df
-
-def save_normalized_and_filtered_knowledge_graph():
-    data_iter = load_data_iterator()
-    gen_df = pd.DataFrame()
-    for chunk in data_iter:
-        df = normalize_df(chunk)
-        df = filter_df(df, False) #our current model doesn't know how to deal with unseen words - and was originally
-                                  # trained on filtered words
-        gen_df = pd.concat((gen_df, df))
-        print(len(gen_df))
-    gen_df.to_csv('data/normalized_knowledge_graph.csv', sep='\t')
+def load_sequence_to_sentence_dataset(h5_path=DEFAULT_SEQ2SENT_DATASET_PATH):
+    return pd.read_hdf(h5_path, 'inputs'), pd.read_hdf(h5_path, 'targets')
 
 
-def create_sequence_to_sentence_iterator(chunksize=50000):
-    for chunk in load_data_iterator(chunksize=chunksize):
-        df = normalize_df(chunk)
-        inputs, outputs = create_input_and_target_from_df(df)
-        yield inputs, outputs
-
-def generate_sequence_to_sentece_dataset():
-    data_it = create_sequence_to_sentence_iterator()
-    total_input_sents = pd.Series()
-    total_target_sents = pd.Series()
-    for input_sents, target_sents in data_it:
-        total_input_sents = pd.concat((total_input_sents, input_sents))
-        total_target_sents = pd.concat((total_target_sents, target_sents))
-        print(len(total_input_sents))
-    return total_input_sents, total_target_sents
-
-def save_sequence_to_sentence_dataset():
-    tot_inputs, tot_ouputs = generate_sequence_to_sentece_dataset()
-    new_df = pd.DataFrame()
-    new_df['inputs'] = tot_inputs
-    new_df['targets'] = tot_ouputs
-    new_df.to_csv('data/filtered_concpetnet_data.csv', sep='\t')
-
-def load_sequence_to_sentence_dataset():
-    df = load_tsv('data/filtered_concpetnet_data.csv')
-    return df['inputs'], df['targets']
-
-def load_normalized_dataset():
-    df = load_tsv('data/normalized_knowledge_graph.csv')
-    return df
-
-def load_tsv(filepath):
-    return pd.read_csv(filepath, sep='\t')
-
-def max_length(tensor):
-    return max(len(t) for t in tensor)
-
-
-def load_numberbatch_weights(filename=NUMBERBATCH, lang=None):
-    df = pd.read_hdf(filename)
+def load_numberbatch(h5_path=DEFAULT_NUMBERBATCH_PATH, lang=None):
+    df = pd.read_hdf(h5_path)
     if lang:
-        mask = df.index.map(lambda x: get_uri_language(x) == lang)
-        df = df[mask]
-        df.reset_index()
+        df = df[df.index.map(lambda x: get_uri_language(x) == lang)]
     return df
+
 
 def create_langauge_index(lang='en'):
-    embeddings = load_numberbatch_weights(lang=lang)
+    embeddings = load_numberbatch(lang=lang)
     words = embeddings.index.map(uri_to_label).values
     return LanguageIndex(words)
 
-class LanguageIndex():
+
+class LanguageIndex:
     def __init__(self, lang):
         self.lang = lang
         self.word2idx = {}
         self.idx2word = {}
-        self.vocab = set()
         self.max_sentence_length = 0
-        self.create_index()
+        self._create_index()
 
-    def create_index(self):
+    def _create_index(self):
+        vocab = set()
         for phrase in self.lang:
             list_of_words = phrase.split(' ')
             self.max_sentence_length = max(self.max_sentence_length, len(list_of_words))
-            self.vocab.update(list_of_words)
-        self.vocab = sorted(self.vocab)
+            vocab.update(list_of_words)
+        vocab = sorted(vocab)
 
         self.word2idx['<pad>'] = 0
-        for index, word in enumerate(self.vocab):
+        self.idx2word[0] = '<pad>'
+        for index, word in enumerate(vocab):
             self.word2idx[word] = index + 1
+            self.idx2word[index + 1] = word
 
-        for word, index in self.word2idx.items():
-            self.idx2word[index] = word
+
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError('{} is not a positive integer '.format(ivalue))
+    return ivalue
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocessing ConceptNet')
     parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=VERSION))
     parser.add_argument('-d', '--dataset', default=DEFAULT_DATASET_PATH, help='Path to ConceptNet dataset')
-    parser.add_argument('-pd', '--processed_dataset', default=DEFAULT_PROCESSED_DATASET_PATH,
-                        help='Path to processed ConceptNet dataset')
-    parser.add_argument('--no_dataset', action='store_true', help='Don\'t process the dataset')
-
+    parser.add_argument('-nd', '--normalized_dataset', default=DEFAULT_NORMALIZED_DATASET_PATH,
+                        help='Path to normalized ConceptNet dataset')
+    parser.add_argument('--no_normalize', action='store_true', help='Don\'t normalize the dataset')
+    parser.add_argument('-sd', '--seq2sent_dataset', default=DEFAULT_SEQ2SENT_DATASET_PATH,
+                        help='Path to seq2sent ConceptNet dataset')
+    parser.add_argument('--no_seq2sent', action='store_true', help='Don\'t create seq2sent dataset')
+    parser.add_argument('--filter_lines', default=1, type=positive_int,
+                        help='For Dev/QA purposes only. Filter lines, only read 1/X lines from the dataset')
+    parser.add_argument('--override', action='store_true', help='Override outputs if already exist')
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
     args = parser.parse_args()
     logging.basicConfig(format='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s',
                         level=logging.DEBUG if args.verbose else logging.INFO)
     logging.info('Starting with args: {}'.format(vars(args)))
 
-    #uncomment this to create the training set
-    # save_sequence_to_sentence_dataset()
-    #creates the dataset to generate sentences from
-    save_normalized_and_filtered_knowledge_graph()
+    preprocessor = _PreProcessor(args.dataset, args.normalized_dataset, args.seq2sent_dataset)
+    if not args.no_normalize:
+        preprocessor.make_normalized_dataset(args.override, args.filter_lines)
+    if not args.no_seq2sent:
+        preprocessor.make_seq2sent_dataset(args.override, args.filter_lines)
