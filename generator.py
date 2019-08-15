2e787d5a85053397f4c881f03dc9bf808146b25e
eliorfrig <2493243+EliorFrig@users.noreply.github.com>
2019-08-15 22:52:30 +0300

    Generator: Added basic filtering of sequences that already in the original dataset, and of weird sentences (according to word counts)

--- generator.py
+++ generator.py
@@ -8,6 +8,7 @@ import numpy as np
 import logging
 import absl.logging
 import argparse
+from collections import defaultdict
 
 
 class NegativeSamplesGenerator:
@@ -16,10 +17,11 @@ class NegativeSamplesGenerator:
         self.dataset = dataset
 
     def generate_random_sentences(self, num_of_sequences, random_state):
-        word_sequences = self.generate_with_same_relation(num_of_sequences, random_state)
-        return word_sequences, self.graph2text.generate_sentences_from_word_sequences(word_sequences)
+        sequences = self._generate_with_same_relation(num_of_sequences, random_state)
+        sentences = self.graph2text.generate_sentences_from_word_sequences(sequences)
+        return sequences, sentences
 
-    def generate_with_same_relation(self, num_of_sequences, random_state):
+    def _generate_with_same_relation(self, num_of_sequences, random_state):
         # the relations might be biased - so it might make sense to sample them by unique
         relations = self.dataset['relation'].sample(num_of_sequences, random_state=random_state)
         result = []
@@ -30,6 +32,32 @@ class NegativeSamplesGenerator:
                 .sample(1, random_state=random_state).item()
             result.append(rel_subject + " " + relation + " " + rel_object)
         return result
+
+
+class GeneratedSamplesFilter:
+    def __init__(self, dataset):
+        self.sequences = list(dataset['subject'] + ' ' + dataset['relation'] + ' ' + dataset['object'])
+
+    def filter_sentences(self, sequences, sentences):
+        for i in reversed(range(len(sentences))):
+            if self._is_sequence_in_dataset(sequences[i]):
+                logging.debug('Removing sequence in original dataset \'{}\''.format(sequences[i]))
+                del sentences[i]
+                del sequences[i]
+            elif self._is_sentence_unreasonable(sentences[i]):
+                logging.debug('Removing unreasonable sentence \'{}\''.format(sentences[i]))
+                del sentences[i]
+                del sequences[i]
+
+    def _is_sequence_in_dataset(self, sequence):
+        return sequence in self.sequences
+
+    @staticmethod
+    def _is_sentence_unreasonable(sentence):
+        word_counts = defaultdict(int)
+        for word in sentence.split():
+            word_counts[word] += 1
+        return max(word_counts.values()) > 3
 
 
 if __name__ == '__main__':
@@ -51,6 +79,7 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--num_of_sentences', type=utils.positive_int, default=2000,
                         help='Number of sentences to generate')
     parser.add_argument('-r', '--random_seed', type=int, help='Random seed to use')
+    parser.add_argument('--no_filter', action='store_true', help='Do not filter badly generated sentences')
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
     args = parser.parse_args()
     # Suppressing absl logger - see https://github.com/tensorflow/tensorflow/issues/26691#issuecomment-507420022
@@ -76,8 +105,18 @@ if __name__ == '__main__':
 
     logging.info('Generating {} random sentences'.format(args.num_of_sentences))
     generator = NegativeSamplesGenerator(graph2text, dataset)
-    random_sequences, random_sentences = generator.generate_random_sentences(args.num_of_sentences,
-                                                                             np.random.RandomState(args.random_seed))
+    gs_filter = GeneratedSamplesFilter(dataset)
+
+    random_sequences = []
+    random_sentences = []
+    random_state = np.random.RandomState(args.random_seed)
+    while len(random_sentences) < args.num_of_sentences:
+        sequences, sentences = generator.generate_random_sentences(args.num_of_sentences - len(random_sentences),
+                                                                   random_state)
+        if not args.no_filter:
+            gs_filter.filter_sentences(sequences, sentences)
+        random_sequences.extend(sequences)
+        random_sentences.extend(sentences)
     logging.info('Done generating, will write outputs to {}'.format(args.out))
     with open(args.out, 'w', encoding='utf-8') as f:
         for seq, sent in zip(random_sequences, random_sentences):
